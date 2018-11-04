21068b26411c12bfabfceb8f05b8c03a043e1dbf
Zev Isert <dev@zevisert.ca>
2018-11-03 21:45:37 -0700

    Travis deploy all builds to surge

--- scripts/staging-build.sh
+++ scripts/staging-build.sh
@@ -3,3 +3,6 @@
 echo "Building.."
 cd source
 gulp build
+
+echo "Deploying to staging server with Surge"
+surge -p ./docs -d https://revela.surge.sh
--- scripts/staging-deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Deploying to staging server with Surge"
-surge -p ./docs -d https://revela.surge.sh
