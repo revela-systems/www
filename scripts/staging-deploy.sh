1d7b9cbed7cfe01f275227d329af73bc91b02b0b
Zev Isert <dev@zevisert.ca>
2018-11-30 12:44:40 -0800

    Should fix travis erroring on staging builds

--- scripts/staging-deploy.sh
+++ scripts/staging-deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "Building sources"
+cd source/ 
 npm run build
 
 echo "Deploying to staging server with Surge"
-npx surge -p docs -d https://revela.surge.sh
+npx surge -p ../docs -d https://revela.surge.sh
