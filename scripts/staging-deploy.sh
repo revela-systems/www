5f596fc79ddf9c3c429735620482cbf35116d828
Zev Isert <dev@zevisert.ca>
2018-11-30 21:01:15 -0800

    Hashbang, not hashslash

--- scripts/staging-deploy.sh
+++ scripts/staging-deploy.sh
@@ -8,4 +8,4 @@ echo "Building sources"
 npm run build
 
 echo "Deploying to staging server with Surge"
-npx surge -p ../docs -d https://revela.surge.sh
+npm surge -p ../docs -d https://revela.surge.sh
